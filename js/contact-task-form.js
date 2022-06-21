c318ae72bb24893511bc6e4689fc279f20ee2a53
Jens Schuppe <schuppe@systopia.de>
2022-06-21 13:23:51 +0200

    Initially toggle QR code parameters depending on selected exporter class

--- js/contact-task-form.js
+++ js/contact-task-form.js
@@ -1,5 +1,9 @@
 CRM.$(function($) {
-  $('#exporter_class').on('change', function() {
+  let $exporter_class = $('#exporter_class');
+
+  $('#qr-code-parameters').toggle($exporter_class.val() === 'CRM_Esr_GeneratorQR');
+
+  $exporter_class.on('change', function() {
     $('#qr-code-parameters').toggle($(this).val() === 'CRM_Esr_GeneratorQR');
   });
 });
